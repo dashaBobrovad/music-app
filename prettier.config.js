03b3035ff2fe1594bc69757835eb560964f90443
dashaBobrovad <dbobrova.dasha@gmail.com>
2025-11-21 18:33:22 +0300

    rewrite code & add and config prettier

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+export default {
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    tabWidth: 2,
+    printWidth: 100,
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    endOfLine: 'lf',
+    jsxWhitespaceSensitivity: "strict",
+    plugins: [
+      'prettier-plugin-organize-imports',
+    ],
+  }
+  
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const data = [
+  {
+    id: 1,
+    title: 'Musicfun soundtrack',
+    url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3',
+  },
+  {
+    id: 2,
+    title: 'Musicfun soundtrack instrumental',
+    url: ' https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3',
+  },
+]
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1> Список треков </h1>
+
+      {data === null ? (
+        <div>loading...</div>
+      ) : data.length === 0 ? (
+        <div>empty</div>
+      ) : (
+        <ul>
+          {data?.map(({ id, title, url }) => (
+            <li key={id}>
+              <span>{title}</span>
+
+              <audio src={url} controls />
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   )
 }
-
-export default App
