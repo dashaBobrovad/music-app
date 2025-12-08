e3deeab47bc46eb8f8acaf5685fa4ec3210950c1
dashaBobrovad <dbobrova.dasha@gmail.com>
2025-12-08 14:49:21 +0300

    refactor

--- backend/src/helpers/sleep.ts
+++ backend/src/helpers/sleep.ts
@@ -1,1 +1,1 @@
-export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, 3000))
+export const sleep = (ms: number = 3000) => new Promise(resolve => setTimeout(resolve, ms))
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
     proxy: {
       '/api': {
